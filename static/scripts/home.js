e583a27548db47cc6ca69b2ae32b6ea48ea168ee
Srikar Prasad <srikasip@gmail.com>
2017-10-23 13:23:18 -0400

    Got rid of a random file, connected to an airtable database and controlled input types

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -1,6 +1,10 @@
 hasContent = false;
 
+incrementor = 0;
+samples = ["strollers", "coffee shops in Providence", "bicycle makers", "hotels in Reykjavik"];
+
 $(document).ready(function(){
+  setButtonEvents();
   setListableEvents();
   requestAnimationFrame(scrollSamples);
 
@@ -33,19 +37,58 @@ function setListableEvents(){
     {
       hasContent = true;
     }
-  });  
+  });
+}
+function setButtonEvents(){
+  $("#rankem").click(function(){
+    searchTerm = $("#listable").val().trim();
+    //if there is no good content, then do nothing
+    if (searchTerm == "" || samples.indexOf(searchTerm) > -1){
+      $(".messageAlert").text("Please provide a valid search");
+      $(".messageAlert").css("display", "block");
+    }
+    //if there is good content, then load it in the database and show an error message and thank them.
+    else{
+
+      $.ajax({
+        method: "POST",
+        url: "https://api.airtable.com/v0/applFlW642fYMqKOj/Table%201",
+        beforeSend: function (xhr) {
+          xhr.setRequestHeader('Authorization', 'Bearer keyqXy9wVseyd2L6S');
+          $("#rankem").attr("disabled", "disabled");
+        },
+        contentType: "application/json",
+        data: '{"fields": {"Search Term": "'+ searchTerm + '"}}'
+      })
+        .done(function( msg ) {
+          $(".messageAlert").text("We are so excited that you want to use our product! We're still working, though. Please check back in a month or so!");
+          $(".messageAlert").css("display", "block");
+          $("#rankem").removeAttr("disabled");
+        })
+        .always(function(msg) {
+          console.log(msg);
+        });
+    }
+  });
 }
 
-incrementor = 0;
-samples = ["strollers", "coffee shops in Providence", "bicycle makers", "hotels in Reykjavik"];
-
-
-
-function scrollSamples()
+function setDefaultStyling()
 {
+  // var disabled_attr = $("#rankem").attr('disabled');
+  // if (typeof disabled_attr == typeof undefined || disabled_attr == false)
+  // {
+  //   $("#rankem").attr('disabled', 'disabled');
+  // }
+
   if (!$("#listable").hasClass("fadedFont")){
     $("#listable").addClass("fadedFont");
   }
+}
+
+function scrollSamples()
+{
+  setDefaultStyling();
+
   index = (incrementor % samples.length);
   phrase = samples[index];
 
